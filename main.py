7cf2557aeadd580827ced1cdb2e5f560078896bd
Flavio Monteiro <>
2022-08-02 21:40:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+  return 'A API está no ar'
+
+@app.route('/vendas')
+def pegarvendas():
+  tabela = pd.read_csv('advertising.csv')
+  total_vendas = tabela['Vendas'].sum()
+  resposta = {'total_vendas':total_vendas}
+  return jsonify(resposta)
+
+  
+
+app.run(host='0.0.0.0')
