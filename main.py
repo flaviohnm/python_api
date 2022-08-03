e92a2a29e71f8fad026e5c989459e340c882d156
Flavio Monteiro <flaviohnm@gmail.com>
2022-08-03 01:49:16 +0000

    Novo deploy

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def pegarvendas():
   total_vendas = tabela['Vendas'].sum()
   resposta = {'total_vendas':total_vendas}
   return jsonify(resposta)
-if __name__ == "__main__":
   
+if __name__ == "__main__":
   app.run(debug=True)
